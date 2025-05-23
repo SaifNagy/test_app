94fc9b1328bcced590f66e5500d14cfc8db36ce2
SAIF-NAGY\SaifNagy <Saaifnagyy@gmail.com>
2025-05-24 00:35:13 +0300

    - added some colors and appbar

--- lib/features/post-details/ui/post_details_screen.dart
+++ lib/features/post-details/ui/post_details_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:test_app/features/post-details/logic/post_details_cubit.dart';
 import 'package:test_app/features/post-details/logic/post_details_state.dart';
 
-
 class PostDetailsScreen extends StatelessWidget {
   final int postId;
   const PostDetailsScreen({super.key, required this.postId});
@@ -13,9 +12,17 @@ class PostDetailsScreen extends StatelessWidget {
     return BlocBuilder<PostDetailsCubit, PostDetailsState>(
       builder: (context, state) {
         return Scaffold(
-          appBar: AppBar(title: const Text('Post Details')),
+          appBar: AppBar(
+            title: Text(
+              'Post Details',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            backgroundColor: Colors.lightBlue.shade50,
+            centerTitle: true,
+            elevation: 2,
+          ),
           body: state.when(
-            initial: () => const SizedBox(), 
+            initial: () => const SizedBox(),
             loading: () => const Center(child: CircularProgressIndicator()),
             success: (post) => Padding(
               padding: const EdgeInsets.all(16.0),
